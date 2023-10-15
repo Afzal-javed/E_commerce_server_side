7d079e5a0ad34c3bbf5547c3bf89289c1e2c1fb1
afzal javed <afzaljaved59832@gmail.com>
2023-10-15 14:16:19 +0530

    Changing the limit for body Parsar

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use((req, res, next) => {
 dotenv.config();
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.json({ limit: '10mb' }));
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
